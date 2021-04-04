526a9858f4166457046d5b22877ff3c052352e91
MarniSaiSanjay <79045301+MarniSaiSanjay@users.noreply.github.com>
2021-04-04 22:23:35 +0530

    may be final one

---
+++ notesapp.js
@@ -0,0 +1,70 @@
+ // Your web app's Firebase configuration.
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ var firebaseConfig = {
+   apiKey: "AIzaSyDKzDTZOMSocDPsd4s6IQWx26ZnIejWzLo",
+   authDomain: "notesapp-72493.firebaseapp.com",
+   projectId: "notesapp-72493",
+   storageBucket: "notesapp-72493.appspot.com",
+   messagingSenderId: "68655298913",
+   appId: "1:68655298913:web:5c8fedf423c27e13252d20",
+   measurementId: "G-D8QWSWYMN9"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics(); 
+ const auth = firebase.auth();
+ 
+ function signUp(){
+	var Email= document.getElementById("email");
+	var pass= document.getElementById("password");
+	const promise = auth.createUserWithEmailAndPassword(Email.value,pass.value);
+	promise.catch(e => alert(e.message +" Try again."));// alert us if any error occur i.e wrong format of email or password is < 6 characters
+	promise.then(user => {
+		console.log(user);
+		alert("Signed Up");
+		window.location.href="final2.html";
+		localStorage.setItem("current",Email.value);
+	})}
+ 
+ function signIn(){
+	var Email=document.getElementById("email");
+	var pass=document.getElementById("password");
+const promise = auth.signInWithEmailAndPassword(Email.value,pass.value);
+promise.catch(e => alert(e.message));
+promise.then(user => {
+	console.log("Signed In as " + Email.value);
+	window.location.href="final2.html";
+	localStorage.setItem("current",Email.value);
+})
+}
+
+function SignOut() {	
+ auth.signOut().then(() => {
+	 alert("Thankyou...");
+	 window.location.href="noteslogin.html";
+	 localStorage.removeItem("current");
+ })
+}
+ function reset() {
+	var emailAddress=document.getElementById("passreset");
+	if(emailAddress !=""){
+		const resetreq = auth.sendPasswordResetEmail(emailAddress.value);
+		resetreq.then(function () {
+			alert("We sent an email with a link to get back into your account.");
+		})
+		resetreq.catch(e => alert(e.message));
+	}
+ }
+
+
+
+
+
+
+
+
+
+
+
+
